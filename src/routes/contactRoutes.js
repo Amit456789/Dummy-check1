1cadfadfae0bce1729fbfdc474b8dc77254b3bc3
Amit Bhandari <amitbhandari@pearlorganisation.com>
2023-06-07 14:33:19 +0530

    Swapped controller and route

--- src/routes/contactRoutes.js
+++ src/routes/contactRoutes.js
@@ -1,27 +1,12 @@
-import { ContactModel } from "../models/contactModal.js";
-import { validationResult } from "express-validator";
-export const ContactCreate = async (req, res) => {
-  const errors = validationResult(req);
-  if (!errors.isEmpty()) {
-    return res.status(200).json({
-      errors: errors.array(),
-    });
-  }
-  //   const { email } = req.body;
-  //   let data = await ContactModel.find({ email });
-  //   if (data) {
-  //     res.status(400).json({
-  //       success: false,
-  //       message:"This email is already registerd please trey with a new one"
-  //   })
-  // }
-  let payload = await ContactModel.create(req.body);
-  res.status(200).json({
-    data: payload,
-  });
-  // res.send("Data");
-};
-export const ContactDetails = async (_, res) => {
-  let data = await ContactModel.find();
-  res.send(data);
-};
+
+import express from "express";
+const router = express.Router();
+
+import { ContactCreate, ContactDetails } from "../controllers/contactController.js"
+import { ContactValidation } from "../Validations/contactValidation.js";
+// import { errors } from "../ErrorHandler/errors.js";
+
+router.post("/contact", ContactValidation, ContactCreate);
+router.get("/contact", ContactDetails);
+
+export { router };
