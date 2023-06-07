6d1b352be7de57d4459e70edb4e175d3c2ec7928
Amit Bhandari <amitbhandari@pearlorganisation.com>
2023-06-07 15:20:04 +0530

    Added api for career page

--- src/models/careerModel.js
+++ src/models/careerModel.js
@@ -5,34 +5,27 @@ const CareerSchema = new mongoose.Schema({
   fname: {
     type: String,
     trim: true,
-    required: true,
   },
   lname: {
     type: String,
     trim: true,
-    required: true,
   },
   city: {
     type: String,
     trim: true,
-    required: true,
   },
   education: {
     type: String,
-    required: true,
     trim: true,
   },
   email: {
     type: String,
-    required: true,
   },
   contact: {
     type: Number,
-    required: true,
   },
   experience: {
     type: Number,
-    required: true,
   },
 });
 export const CareerModel = mongoose.model("Carrer", CareerSchema);
