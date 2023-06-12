260842c822a142b8a3448dc33de61bcd93344db3
Amit456789 <132435755+Amit456789@users.noreply.github.com>
2023-06-12 14:47:52 +0530

    Update contactValidation.js

--- src/Validations/contactValidation.js
+++ src/Validations/contactValidation.js
@@ -3,11 +3,11 @@ import { body } from "express-validator";
 export const ContactValidation = [
   body("email").isEmail().withMessage("Please provide a valid email address"),
   body("fname")
-    .isLength({ min: 2, max: 20 })
-    .withMessage("fname must be between 8 and 20 characters long"),
+    .isLength({ min:2 , max: 40 })
+    .withMessage("fname must be between 2 and 40 characters long"),
   body("lname")
-    .isLength({ min: 2, max: 20 })
-    .withMessage("lname must be between 8 and 20 characters long"),
+    .isLength({ min: 2, max: 40 })
+    .withMessage("lname must be between 2 and 40 characters long"),
   body("phone")
     .isLength(10)
     .withMessage("phone number must be of 10 characters"),
