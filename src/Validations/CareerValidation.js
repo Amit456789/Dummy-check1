a11bf7c2dbeca183f7cef152bb091d25bbbe92c1
Amit Bhandari <amitbhandari@pearlorganisation.com>
2023-06-14 14:37:23 +0530

    Errors resolved

--- src/Validations/CareerValidation.js
+++ src/Validations/CareerValidation.js
@@ -1,19 +1,43 @@
-import { body } from "express-validator";
+// import { body } from "express-validator";
 
-export const CareerValidation = [
-  body("email").isEmail().withMessage("Please provide a valid email address"),
-  body("fname")
-    .isLength({ min: 2, max: 20 })
-    .withMessage("fname must be between 8 and 20 characters long"),
-  body("lname")
-    .isLength({ min: 2, max: 20 })
-    .withMessage("lname must be between 8 and 20 characters long"),
-  body("phone")
-    .notEmpty()
-    .withMessage("Phone Number is required")
-    .isLength(10)
-    .withMessage("phone number must be of 10 characters"),
-  body("city").notEmpty().withMessage("City name is required"),
-  body("phone").notEmpty().withMessage("City name is required"),
-  body("cv").notEmpty()
-];
+// export const CareerValidation = [
+//   body("email").isEmail().withMessage("Please provide a valid email address"),
+//   body("fname")
+//     .isLength({ min: 2, max: 20 })
+//     .withMessage("fname must be between 8 and 20 characters long"),
+//   body("lname")
+//     .isLength({ min: 2, max: 20 })
+//     .withMessage("lname must be between 8 and 20 characters long"),
+//   body("phone")
+//     .notEmpty()
+//     .withMessage("Phone Number is required")
+//     .isLength(10)
+//     .withMessage("phone number must be of 10 characters"),
+//   body("city").notEmpty().withMessage("City name is required"),
+//   body("phone").notEmpty().withMessage("City name is required"),
+//   // body("cv").notEmpty()
+// ];
+// // import { body } from "express-validator";
+// export const CareerValidation = [
+//   body("data.fname")
+//     .isLength({ min: 2, max: 20 })
+//     .withMessage("fname must be between 2 and 20 characters long"),
+//   body("data.lname")
+//     .isLength({ min: 2, max: 20 })
+//     .withMessage("lname must be between 2 and 20 characters long"),
+
+import Joi from "joi";
+const schema = Joi.object({
+  email: Joi.email(),
+  fname: Joi.string().min(2).max(40),
+  lname: Joi.string().min(2).max(40),
+  phone: Joi.number().integer().min(1000000000).max(9999999999).required(),
+  city:Joi.string().empty()
+});
+export const careerValidation = (data) => {
+  console.log("Data", data);
+  const result = schema.validate(data);
+  return result;
+};
+
+//  default careerValidation;
--- src/routes/CareerRoute.js
+++ src/routes/CareerRoute.js
@@ -1,13 +1,10 @@
 import express from "express";
 const router = express.Router();
 
-import {
-  CareerCreate,
-  // CareerDetails,
-  // saveCareerDetails,
-} from "../controllers/careerController.js";
+import { CareerCreate } from "../controllers/careerController.js";
 
 import { validateMimeType } from "../utils/multerUpload.js";
+import { CareerValidation } from "../Validations/CareerValidation.js";
 // Post route to post a new data from user including cv
 router.post("/career", validateMimeType, CareerCreate);
 
