ce6ee05209416ca12eaaac7a1546099d3bf55c75
Amit Bhandari <amitbhandari@pearlorganisation.com>
2023-06-19 12:19:06 +0530

    Exception handling

--- src/controllers/careerController.js
+++ src/controllers/careerController.js
@@ -3,8 +3,6 @@ const CareerModel = require("../models/careerModel.js").CareerModel;
 const careerValidation =
   require("../Validations/CareerValidation.js").careerValidation;
 
-
-
 exports.CareerCreate = async (req, res) => {
   // const errors = validationResult(req);
   // if (!errors.isEmpty()) {
@@ -14,14 +12,14 @@ exports.CareerCreate = async (req, res) => {
   // }
   const datum = JSON.parse(req.body.document);
   console.log("This is body", datum.city);
-  // let result = careerValidation(datum);
+  let result = careerValidation(datum);
   // console.log("error", result.error);
-  // if (result.error) {
-  //   return res.status(200).json({
-  //     success: false,
-  //     msg: result.error,
-  //   });
-  // }
+  if (result.error) {
+    return res.status(200).json({
+      success: false,
+      msg: result.error,
+    });
+  }
   datum.cv = `${process.env.URL}/public/${req?.file?.filename}`;
 
   // let { fname, lname, city, education, email, contact, experience ,phone} = req.body;
@@ -37,11 +35,18 @@ exports.CareerCreate = async (req, res) => {
   //   phone,
   //   cv: `${process.env.URL}/public/${req?.file?.filename}`,
   // };
-  let payload = await CareerModel.create(datum);
-  console.log("uploadeObj",payload);
-  res.status(200).json({
-    data: payload,
-  });
+  try {
+    let payload = await CareerModel.create(datum);
+    console.log("uploadeObj", payload);
+    res.status(200).json({
+      data: payload,
+    });
+  } catch (error) {
+    res.status(200).json({
+      status: false,
+      Error: error.message,
+    });
+  }
 };
 exports.CareerDetails = async (req, res) => {
   const errors = validationResult(req);
@@ -51,11 +56,18 @@ exports.CareerDetails = async (req, res) => {
     });
   }
   // let payload = {...data,req.body}
-  console.log("req.body=========================", req.body);
-  console.log("req.files=========================", req.files);
+  // console.log("req.body=========================", req.body);
+  // console.log("req.files=========================", req.files);
   let payload = await CareerModel.find();
+  try {
+    res.status(200).json({
+      data: payload,
+    });
+  } catch (error) {
+    res.status(200).json({
+      status: false,
+      Error: error.message,
+    });
+  }
   // console.log(payload)
-  res.status(200).json({
-    data: payload,
-  });
 };
