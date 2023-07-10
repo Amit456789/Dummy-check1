6fb009b91e65f1ea7e11b7892329a0286148a22f
Amit Bhandari <amitbhandari@pearlorganisation.com>
2023-07-10 17:20:27 +0530

    Get in touch route solved

--- src/routes/CareerRoute.js
+++ src/routes/CareerRoute.js
@@ -1,13 +1,3 @@
-// import express from "express";
-// const router = express.Router();
-
-// import { CareerCreate } from "../controllers/careerController.js";
-
-// import { validateMimeType } from "../utils/multerUpload.js";
-// // Post route to post a new data from user including cv
-// router.post("/career", validateMimeType, CareerCreate);
-
-// export { router };
 
 
 //ES5
@@ -19,7 +9,7 @@ const multerUploadUtils = require("../utils/multerUpload.js");
 
 const CareerCreate = careerController.CareerCreate;
 const validateMimeType = multerUploadUtils.validateMimeType;
-
+console.log("INSDIDE CAREER")
 // Post route to post a new data from user including cv
 router.post("/", validateMimeType, CareerCreate);
 router.get("/", careerController.CareerDetails);
--- src/routes/GetInTouch.js
+++ src/routes/GetInTouch.js
@@ -3,7 +3,7 @@ const express = require("express");
 const { CreateGetInTouch } = require("../controllers/GetInTouchController");
 const router = express.Router();
 
-
+console.log("GET in TOUCH")
 router.post("/touch", CreateGetInTouch);
 // router.get("/touch", ContactDetails);
 
